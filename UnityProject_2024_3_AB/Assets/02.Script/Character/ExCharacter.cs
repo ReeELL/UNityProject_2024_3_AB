0edc7358b1c6468136fda11d00d964283431d28b
ReeELL <reeel1001@gmail.com>
2024-03-27 10:30:10 +0900

    Feet:"Xml까지"

---
+++ UnityProject_2024_3_AB/Assets/02.Script/Character/ExCharacter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExCharacter : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private void Update()
+    {
+        Move();
+    }
+    protected virtual void Move()
+    {
+        transform.Translate(
+            Vector3.forward * speed * Time.deltaTime);
+    }
+    public void DestoryCharacter()
+    {
+        Destroy(gameObject);
+    }
+}
